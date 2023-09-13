6a142c6b71d574b0fda8b4ca8bfa471a0d5d45df
Victor <victorbultez2@gmail.com>
2023-09-13 10:14:19 +0200

    merge of main file & reformat to python standards

--- main.py
+++ main.py
@@ -4,14 +4,16 @@
 from numpy.random import randint
 from numpy.random import rand
 
+
 # the objective is having the biggest value possible
 def onemax(x):
     return -sum(x)
 
+
 # pick 2 random parent and keep the best one
 def selection(pop, scores, k=3):
     selection_ix = randint(len(pop))
-    for ix in randint(0, len(pop), k-1):
+    for ix in randint(0, len(pop), k - 1):
         if scores[ix] < scores[selection_ix]:
             selection_ix = ix
     return pop[selection_ix]
@@ -21,7 +23,7 @@ def selection(pop, scores, k=3):
 def crossover(p1, p2, r_cross):
     c1, c2 = p1.copy(), p2.copy()
     if rand() < r_cross:
-        pt = randint(1, len(p1)-2)
+        pt = randint(1, len(p1) - 2)
         c1 = p1[:pt] + p2[pt:]
         c2 = p2[:pt] + p1[pt:]
     return [c1, c2]
@@ -41,12 +43,12 @@ def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut):
     best, best_eval = 0, objective(pop[0])
 
     for gen in range(n_iter):
-        scores = [objective(c) for c in pop] # score of each bitstring
+        scores = [objective(c) for c in pop]  # score of each bitstring
         # keep track of current best score
         for i in range(n_pop):
             if scores[i] < best_eval:
                 best, best_eval = pop[i], scores[i]
-                print(">%d, new best f(%s) = %.3f" % (gen,  pop[i], scores[i]))
+                print(">%d, new best f(%s) = %.3f" % (gen, pop[i], scores[i]))
 
         # we select the best bitstrings in a tournament like selection
         selected = [selection(pop, scores) for _ in range(n_pop)]
@@ -54,20 +56,37 @@ def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut):
         # generate new children
         children = list()
         for i in range(0, n_pop, 2):
-            p1, p2 = selected[i], selected[i+1]
-            for c in crossover(p1, p2, r_cross): # we mix the 2 parents
-                mutation(c, r_mut) # we create mutation among the child
+            p1, p2 = selected[i], selected[i + 1]
+            for c in crossover(p1, p2, r_cross):  # we mix the 2 parents
+                mutation(c, r_mut)  # we create mutation among the child
                 children.append(c)
-        pop = children # we replace the population with the new children
+        pop = children  # we replace the population with the new children
     return [best, best_eval]
 
+
 # params
-n_iter = 100 # number of iteration
-n_bits = 20 # number of bits inside the bitstrings
-n_pop = 100 # population size
-r_cross = 0.9 # probability of crossover
-r_mut = 1.0 / float(n_bits) # probability of mutation
+n_iter = 100  # number of iteration
+n_bits = 20  # number of bits inside the bitstrings
+n_pop = 100  # population size
+r_cross = 0.9  # probability of crossover
+r_mut = 1.0 / float(n_bits)  # probability of mutation
 
 best, score = genetic_algorithm(onemax, n_bits, n_iter, n_pop, r_cross, r_mut)
 print('Done!')
 print('f(%s) = %f' % (best, score))
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def chessboard(n):
+    for i in range(len(n)):
+        if (i == n[i] - 1):
+            print("|Q|\n")
+        else:
+            print("| |\n")
